45c03e1104cb665bac38d02bd32c737d72a700ba
최근원 <kidjustinchoi@gmail.com>
2024-04-16 17:29:23 +0900

    feat: add tailwind css and remove @emotion style

---
+++ src/css/index.ts
@@ -0,0 +1,3 @@
+export * from './reset.css';
+export * from './index.css';
+export * from './global.css';
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { Global } from '@emotion/react';
+import './css/index';
 
 import { App } from './App';
-import { globalStyle } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Global styles={[globalStyle]} />
     <App />
   </React.StrictMode>,
 );
